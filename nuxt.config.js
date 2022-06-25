9fc964e6efb629eb8596411665b9d2908c4b024c
Roman <guivanrv@gmail.com>
2022-06-25 10:06:53 +0100

    build 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/guivarom/ios-games/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'ios-games',
